7362b3478103242496c6d1f32c6517733258e5bf
Slaiman <sulaimanahmad321@gmail.com>
2017-03-26 16:24:15 +0100

    Completed s2c11

--- session2/challenge/11_classes.rb
+++ session2/challenge/11_classes.rb
@@ -12,9 +12,41 @@
 # Zero bottles of beer on the wall.
 #
 # Your program should not use ninety-nine output statements!
-# Design your program with a class named BeerSong whose initialize method 
+# Design your program with a class named BeerSong whose initialize method
 # receives a parameter indicating the number of bottles of beer initially on the wall.
 # If the parameter is less than zero, set the number of bottles to zero. Similarly,
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+require 'numbers_and_words'
+class BeerSong
+  attr_accessor :beers
+
+  def initialize(beers)
+    beers = 0  if beers < 0
+    beers = 99 if beers > 99
+    self.beers = beers
+  end
+
+  def print_song
+    beers.downto 1 do |i|
+      print_stanza i
+    end
+  end
+
+  def print_stanza(n)
+    if n.zero?
+      String.new
+    else
+      puts "#{n.to_words.capitalize} #{bottle n} of beer on the wall,"        ,
+           "#{n.to_words.capitalize} #{bottle n} of beer,"                    ,
+           "Take one down, pass it around,"                         ,
+           "#{(n.to_i - 1).to_words.capitalize} #{bottle n-1} of beer on the wall."
+    end
+  end
+
+
+  def bottle(n)
+    if n == 1 then 'bottle' else 'bottles' end
+  end
+end

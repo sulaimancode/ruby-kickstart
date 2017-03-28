8a3c43fbed66f56a71efbf6d7d50f2a2757927c1
Slaiman <sulaimanahmad321@gmail.com>
2017-03-28 14:14:41 +0100

    Completed s3c3

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,9 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+def word_count(str)
+  arr = str.downcase.split
+  hash = Hash.new { |this_hash, key| this_hash[key] = arr.count(key)}
+  arr.each { |word| hash[word]}
+  hash
+end
